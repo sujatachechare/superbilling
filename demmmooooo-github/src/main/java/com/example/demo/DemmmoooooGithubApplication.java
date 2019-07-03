7167ca6e6032cd8c93a37a324c8ebb6c872c39f6
sujatachechare <sujata28chechare@gmail.com>
2019-07-03 15:48:55 +0530

    complete

---
+++ demmmooooo-github/src/main/java/com/example/demo/DemmmoooooGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemmmoooooGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemmmoooooGithubApplication.class, args);
+	}
+
+}
